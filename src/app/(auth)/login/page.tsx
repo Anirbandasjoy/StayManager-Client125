a068f2033e125582a8b4730f02edfb3dc3cb59a8
Anirbandasjoy <joy600508@gmail.com>
2024-06-24 17:36:15 +0600

    design login page

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,7 +1,55 @@
-import React from "react";
-
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 const Login = () => {
-  return <div>Login page</div>;
+  return (
+    <div className="flex h-screen w-full justify-center items-center bg-green-100">
+      <Card className="w-[350px] bg-blue-300 shadow-xl">
+        <CardHeader>
+          <CardTitle>Please Login</CardTitle>
+          <CardDescription>
+            Already created Account please login.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form>
+            <div className="grid w-full items-center gap-4">
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" placeholder="Enter your email address.." />
+              </div>
+              <div className="flex flex-col space-y-1.5">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  type="password"
+                  id="password"
+                  placeholder="Enter your Password"
+                />
+              </div>
+            </div>
+          </form>
+        </CardContent>
+        <CardFooter className="w-full">
+          <Button className="w-full">Login</Button>
+        </CardFooter>
+      </Card>
+    </div>
+  );
 };
 
 export default Login;
